e41887dbfabdb230743806e6971e418db2c07d5f
Kaixinguo <kaixinguo@kaixinguo.site>
2019-10-01 04:26:58 +0800

    Add pull.sh

--- bin/pull.sh
+++ bin/pull.sh
@@ -2,7 +2,24 @@
 #i:Fetch from and integrate with another repository or a local branch
 #u:Usage: rgit pull
 
-PWD=$(./lib/traverse.sh $CURRENT)
+ROOT=$(./lib/traverse.sh $CURRENT)
 R=$?;[ $R != 0 ]&&exit $R
 
-echo $PWD
+RSYNC=$ROOT/.rsync
+
+REMOTE=$(cat $RSYNC/remote) || exit 1
+CONFIG=$(cat $RSYNC/config) || exit 1
+
+EXCLUDE="--exclude=.rsync"
+if [ -f "$RSYNC/exclude" ];then
+    while read LINE
+    do
+        LINE=${LINE##* }
+        if [[ "$LINE" != "" && "${LINE:0:1}" != "#" ]];then
+            EXCLUDE="--exclude=$LINE "$EXCLUDE
+        fi
+    done < $RSYNC/exclude
+fi
+
+CMD="rsync $CONFIG $EXCLUDE $REMOTE/ $ROOT/"
+$CMD
