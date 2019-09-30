6465eb6c57cfd4efa789ca5a5f056d008ff17705
Kaixinguo <kaixinguo@kaixinguo.site>
2019-10-01 04:26:02 +0800

    Add push.sh

--- bin/push.sh
+++ bin/push.sh
@@ -5,4 +5,21 @@
 ROOT=$(./lib/traverse.sh $CURRENT)
 R=$?;[ $R != 0 ]&&exit $R
 
-echo $PWD
+RSYNC=$ROOT/.rsync
+
+REMOTE=$(cat $RSYNC/remote) || exit 1
+CONFIG=$(cat $RSYNC/config) || exit 1
+
+EXCLUDE="--exclude=.rsync"
+if [ -f "$RSYNC/exclude" ];then
+    while read LINE
+    do
+        LINE=${LINE##* }
+        if [[ "$LINE" != "" && "${LINE:0:1}" != "#" ]];then
+            EXCLUDE="--exclude=$LINE "$EXCLUDE
+        fi
+    done < $RSYNC/exclude
+fi
+
+CMD="rsync $CONFIG $EXCLUDE $ROOT/ $REMOTE/"
+$CMD
