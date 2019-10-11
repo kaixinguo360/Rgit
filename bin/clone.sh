267df8a59081f7850e59989488c1a7499008856d
Kaixinguo <kaixinguo@kaixinguo.site>
2019-10-11 16:33:06 +0800

    Add clone.sh

---
+++ bin/clone.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+#i:Clone a repository into a new directory
+#u:Usage: rgit clone <repo> [<dir>]
+
+REMOTE=$1
+DIR=$2
+
+if [ "$REMOTE" = "" ];then
+    echo "rgit: missing operand"
+    print_help
+    exit 1
+fi
+if [ "${REMOTE%?}/" = "$REMOTE" ];then
+    REMOTE=${REMOTE%?}
+fi
+
+if [ "$DIR" = "" ];then
+    DIR=${REMOTE##*/}
+fi
+if [ "${DIR%?}/" = "$DIR" ];then
+    DIR=${DIR%?}
+fi
+
+# Get Absolute Path
+cd $CURRENT
+DIR=$(realpath $DIR)
+if [ -z "$(echo "$REMOTE"|grep ':')" ];then
+    REMOTE=$(realpath $REMOTE)
+fi
+cd $RGIT_ROOT
+
+# Check Target Directory
+if [ -d "$DIR" ];then
+    echo "fatal: destination path '$DIR' already exists and is not an empty directory."
+    exit 1
+else
+    mkdir -p $DIR
+fi
+
+# Clone Repository
+echo "Cloning into '${DIR##*/}'..."
+echo "Remote: $REMOTE"
+echo "Local:  $DIR"
+cd $DIR
+$RGIT_ROOT/rgit init $REMOTE
+$RGIT_ROOT/rgit pull
--- bin/init.sh
+++ bin/init.sh
@@ -19,7 +19,7 @@ mkdir "$RSYNC"
 echo "$REMOTE" > $RSYNC/remote
 echo "-a -v -zz --delete" > $RSYNC/config
 
-echo ".git" > $RSYNC/exclude
+echo "" > $RSYNC/exclude
 if [[ -f "$CURRENT/.gitignore" && "$(sed -n '/.rsync/'p $CURRENT/.gitignore)" = "" ]];then
     echo ".rsync" >> $CURRENT/.gitignore
 fi
