d1fa8dc51022e8014bf00bb42935948614614afe
Kaixinguo <kaixinguo@kaixinguo.site>
2020-12-27 14:02:31 +0800

    Optimizr pull and push

--- bin/push.sh
+++ bin/push.sh
@@ -2,21 +2,22 @@
 #i:Update remote refs along with associated objects
 #u:Usage: rgit push
 
-ROOT=$(traverse $CURRENT) || exit
-RSYNC=$ROOT/.rsync
+ROOT="$(traverse $CURRENT)" || exit
+RSYNC="$ROOT/.rsync"
 
-REMOTE=$(cat $RSYNC/remote) || exit
-CONFIG=$(cat $RSYNC/config) || exit
+REMOTE="$(cat $RSYNC/remote)" || exit
+CONFIG="$(cat $RSYNC/config)" || exit
 
 EXCLUDE="--exclude=.rsync"
 if [ -f "$RSYNC/exclude" ];then
-    EXCLUDE="--exclude-from=$RSYNC/exclude "$EXCLUDE
+    EXCLUDE="--exclude-from=$RSYNC/exclude $EXCLUDE"
 fi
 
-BACKUP_PATH=.rsync/backup/$(date '+%Y/%m/%d/%H%M%S')
+BACKUP_PATH=".rsync/backup/$(date '+%Y/%m/%d/%H%M%S')"
 BACKUP="-b --backup-dir $BACKUP_PATH"
 
 CMD="rsync $@ $BACKUP $CONFIG $EXCLUDE $ROOT/ $REMOTE/"
+
 if [ -f "$RSYNC/hooks/pre-push" ];then
     PRE_CMD="$(cat $RSYNC/hooks/pre-push|grep -Ev '^$'|tr '\n' ';')"
 fi
@@ -30,10 +31,10 @@ if [ -f "$RSYNC/hooks/post-push-remote" ];then
     POST_REMOTE_CMD="$(cat $RSYNC/hooks/post-push-remote|grep -Ev '^$'|tr '\n' ';')"
 fi
 
-cd $ROOT
+cd "$ROOT"
 [ -n "$PRE_CMD" ] && { bash -ic "$PRE_CMD"||exit; }
 [ -n "$PRE_REMOTE_CMD" ] && { $RGIT_ROOT/rgit run $PRE_REMOTE_CMD||exit; }
-$CMD||exit
+sh -c "$CMD"||exit
 [ -n "$POST_CMD" ] && { bash -ic "$POST_CMD"||exit; }
 [ -n "$POST_REMOTE_CMD" ] && { $RGIT_ROOT/rgit run $POST_REMOTE_CMD||exit; }
 exit 0
